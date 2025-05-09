1722253a647b71b58dc510ec10c67da176f262e2
Lars Timmerman <infigoforza@gmail.com>
2025-05-09 20:52:49 +0200

    Add read_chunk

--- convert.rb
+++ convert.rb
@@ -30,13 +30,23 @@ class Convert
   def self.validate_png(file)
     # https://www.w3.org/TR/png/#3PNGsignature
     hex_png_signature = "89 50 4E 47 0D 0A 1A 0A".split.map { |hex| hex.to_i(16) }
-
     return if file.read(8).bytes == hex_png_signature
 
     puts "Invalid PNG signature"
     exit
   end
 
+
+  # https://www.w3.org/TR/png/#5Chunk-layout
+  def self.read_chunk(file)
+    # unsigned integer big endian
+    length = file.read(4).unpack1("N")
+    type = file.read(4)
+    data = file.read(length)
+    # Not using it for now
+    crc = file.read(4)
+    [type, data]
+  end
 end
 
 puts Convert.parse_file
